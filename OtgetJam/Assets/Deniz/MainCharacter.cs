b656dc224e70af458a081879a598d19c34742fa4
denzi <denizceylan2002@gmail.com>
2025-04-26 01:31:16 +0300

    su fiziği

--- OtgetJam/Assets/Deniz/MainCharacter.cs
+++ OtgetJam/Assets/Deniz/MainCharacter.cs
@@ -4,6 +4,8 @@ public class MainCharacter : Entity
 {
     [SerializeField] int speed;
 
+    private string currentLayer = "";
+
     protected override void Start()
     {
         base.Start();
@@ -15,20 +17,41 @@ public class MainCharacter : Entity
     {
 
         Vector2 move_v2 = Vector2.zero;
+        currentLayer = base.checkCurrentLayer(); // þu an bulunan layer
+        updateGravity();//suda ve havada olma durumuna göre gravity scaleýný günceller.
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            move_v2 = Vector2.up;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            move_v2 = Vector2.down;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            move_v2 = Vector2.right;
-        }
-        base.moveVectorized(move_v2, speed);
 
+        if (currentLayer == "Water")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                move_v2 = Vector2.up;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                move_v2 = Vector2.down;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                move_v2 = Vector2.right;
+            }
+            base.moveVectorized(move_v2, speed);
+        }
+        
+
+        
     }
+
+    private void updateGravity()
+    {
+        string layerName = base.checkCurrentLayer();
+
+        if (layerName == "Water")//Water ise gravity 0 olcak 
+            base.setGravity(0);
+        else if (layerName == "Air")
+            base.setGravity(1);
+        
+    }
+
+    
 }
